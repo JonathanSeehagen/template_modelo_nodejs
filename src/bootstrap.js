3453f81dee60265c5610d761859b51d082b72fab
Jonathan <jonathanseehagen@gmail.com>
2020-04-30 00:25:38 -0300

    first commit

---
+++ src/bootstrap.js
@@ -0,0 +1,9 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+});
+
+// import dotenv from 'dotenv';
+
+// dotenv.config({
+//  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+// });
